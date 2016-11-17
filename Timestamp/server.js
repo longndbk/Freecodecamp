27087d4b5780c2a43e6487011760e33adfea6908
LongBK <nguyenduclong12a2nd@gmail.com>
2016-11-17 11:56:50 +0700

    Done

--- Timestamp/server.js
+++ Timestamp/server.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var app = express();
 
-app.get ('/', function(req, res){
-  console.log("Hello world!");
-});
+app.use(express.static(__dirname ));
 
-app.listen(8800, function(){
-  console.log("Example app listening port 8800");
+app.get("/:str", function (req, res) {
+	// var pURL = url.parse(req.url, true);
+	var result = {};
+	var str = req.params.str;
+	if (/^[0-9]+$/.test(str) == true){
+		str = parseInt(str);
+	}
+	var date = new Date(str);
+	if (date.getTime()){
+		result.unix = date.getTime();
+		result.natural = date.toDateString();
+	}
+	else{
+		result.unix = null;
+		result.natural = null;
+	}
+	res.json(result);
+	res.end();
 });
+app.listen(process.env.PORT || 1010);
